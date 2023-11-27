611c523bcdded8e7b61132f64cc7b3d9c1a3730f
Chan Lee <Chanaomrithlee@gmail.com>
2023-11-27 10:25:46 -0800

    removed spaces

--- main.js
+++ main.js
@@ -3,31 +3,25 @@
 function renderCoffee(coffee) {
     let coffeeDiv = document.createElement('div');
     coffeeDiv.classList.add('coffee');
-
     let nameDiv = document.createElement('div');
     nameDiv.textContent = coffee.name;
     nameDiv.classList.add('coffee-name'); // Add a class for the coffee name for styling purposes
     coffeeDiv.appendChild(nameDiv);
-
     let roastDiv = document.createElement('div');
     roastDiv.textContent = coffee.roast;
     roastDiv.classList.add('coffee-roast'); // Add a class for the coffee roast for styling purposes
     coffeeDiv.appendChild(roastDiv);
-
     return coffeeDiv;
 }
 
 function renderCoffees(coffees) {
     // Sort the coffees array by id in ascending order
     coffees.sort((a, b) => a.id - b.id);
-
     let coffeeContainer = document.getElementById('coffees');
     coffeeContainer.innerHTML = '';
-
     for (let i = 0; i < coffees.length; i++) {
         coffeeContainer.appendChild(renderCoffee(coffees[i]));
     }
-
     // Update the datalist with coffee names
     updateCoffeeList(coffees);
 }
@@ -35,7 +29,6 @@ function renderCoffees(coffees) {
 function updateCoffeeList(coffees) {
     let coffeeList = document.getElementById('coffee-list');
     coffeeList.innerHTML = '';
-
     coffees.forEach(coffee => {
         let option = document.createElement('option');
         option.value = coffee.name;
@@ -48,7 +41,6 @@ function updateAndRenderCoffees(selectedRoast, searchQuery) {
         (selectedRoast === 'all' || coffee.roast === selectedRoast) &&
         (searchQuery === '' || coffee.name.toLowerCase().includes(searchQuery))
     );
-
     renderCoffees(filteredCoffees);
 }
 
@@ -57,7 +49,6 @@ function handleButtonClick(e) {
     // Check if the clicked element is a button or an image button
     if (e.target.tagName === 'BUTTON' || (e.target.tagName === 'IMG' && e.target.parentElement.tagName === 'BUTTON')) {
         e.preventDefault(); // Prevent the default form submission behavior
-
         // Get the selected roast from the clicked button's id or image button's parent button's id
         let selectedRoast = e.target.id || e.target.parentElement.id;
         let searchQuery = coffeeSearch.value.toLowerCase();
@@ -69,24 +60,19 @@ function handleButtonClick(e) {
 // Function to handle adding a new coffee
 function addNewCoffee(e) {
     e.preventDefault();
-
     // Get values from the form
     const newCoffeeName = document.getElementById('newCoffeeName').value.trim();
     const newCoffeeRoast = document.getElementById('newCoffeeRoast').value;
-
     // Validate that the name is not empty
     if (!newCoffeeName) {
         alert('Please enter a valid coffee name.');
         return;
     }
-
     // Add the new coffee to the array
     const newCoffee = {id: coffees.length + 1, name: newCoffeeName, roast: newCoffeeRoast};
     coffees.push(newCoffee);
-
     // Render the updated list of coffees
     renderCoffees(coffees);
-
     // Clear the form
     document.getElementById('addCoffeeForm').reset();
 }
@@ -115,7 +101,6 @@ const coffeeSearch = document.querySelector('#coffee-search');
 const submitButton = document.getElementById('submit');
 const addCoffeeForm = document.getElementById('addCoffeeForm');
 
-
 addCoffeeForm.addEventListener('submit', addNewCoffee);
 roastButtons.addEventListener('click', handleButtonClick);
 submitButton.addEventListener('click', function (e) {
@@ -128,5 +113,4 @@ submitButton.addEventListener('click', function (e) {
 
 // Initial rendering of coffees
 renderCoffees(coffees);
-
 })();
